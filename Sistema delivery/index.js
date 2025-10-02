bc48284d6d694b8783898701b10116673d8678ea
math45rosa-crypto <Matheus45rosa@gmail.com>
2025-10-02 20:26:22 -0300

    crud-restaurants

--- Sistema delivery/index.js
+++ Sistema delivery/index.js
@@ -1,4 +1,4 @@
-/*const express = require('express')
+const express = require('express')
 const cors = require('cors')
 
 const app = express()
@@ -6,15 +6,12 @@ app.use(cors())
 app.use(express.json())
 
 // TODO: Membro 1 - Importar e mapear
-const  = require('./routes/');
-app.use();
+const restaurantRoutes = require('./routes/restaurantRoutes');
+app.use('/restaurants', restaurantRoutes);
 
-// TODO: Membro 1 - Importar e mapear
-const  = require('./routes/');
-app.use();
 
 app.listen(3000, () => {
 console.log(`Servidor rodando em http://localhost:3000`);
 })
-*/
+
 
