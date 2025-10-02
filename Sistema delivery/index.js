b3ea86c516c5075612faede4ab10a33a57c1a643
DaviJesus66 <davi.jesus@iesb.edu.br>
2025-10-02 04:47:44 -0300

    inicio

---
+++ Sistema delivery/index.js
@@ -0,0 +1,20 @@
+/*const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// TODO: Membro 1 - Importar e mapear
+const  = require('./routes/');
+app.use();
+
+// TODO: Membro 1 - Importar e mapear
+const  = require('./routes/');
+app.use();
+
+app.listen(3000, () => {
+console.log(`Servidor rodando em http://localhost:3000`);
+})
+*/
+
